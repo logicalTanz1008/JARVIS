a9f2300ff53503498519af88cfde1449ec9f74ec
Tanzeel Rahman <67785586+logicalTanz1008@users.noreply.github.com>
2020-10-11 13:08:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const microphoneButton = document.getElementById("microphoneButton");
+
+microphoneButton.addEventListener("click", function () {
+	window.open(
+		"https://logicaltanz1008.github.io/JARVIS/mic",
+		"nativeWindow",
+		"width=350,height=300"
+	);
+});
